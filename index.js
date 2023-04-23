71335808b31ec1f3048009c4b84129a008dccda1
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2023-04-23 11:49:46 +0530

    Parameters added
    Changed into a task tracker app. Added a more categorized version of the app after usage/testing. Dept, task, and time are the newly created elements.

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
-const inputFieldEl = document.getElementById("input-field")
+const deptFieldEl = document.getElementById("dept-field")
+const taskFieldEl = document.getElementById("task-field")
+const timeFieldEl = document.getElementById("time-field")
+
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+
+
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = []
+    
+    inputValue.push(timeFieldEl.value)
+    inputValue.push(deptFieldEl.value)
+    inputValue.push(taskFieldEl.value)
+    
+    console.log(inputValue)
     
     push(shoppingListInDB, inputValue)
     
@@ -27,8 +38,30 @@ onValue(shoppingListInDB, function(snapshot) {
     
         clearShoppingListEl()
         
+        itemsArray.sort();
+        itemsArray.sort((a, b) => {
+                const aTime = a[1][0];
+                const bTime = b[1][0];
+                if (aTime.includes(':') && bTime.includes(':')) {
+                    // Both times are in the format of 'HH:MM{AM/PM}, DD Month'.
+                    // We will convert them to a sortable format of 'YYYY-MM-DDTHH:MM:00'.
+                    const aDate = new Date(`${aTime.slice(-2)} ${aTime.slice(0, -6)} ${aTime.slice(-5, -3)} 2023`);
+                    const bDate = new Date(`${bTime.slice(-2)} ${bTime.slice(0, -6)} ${bTime.slice(-5, -3)} 2023`);
+                    return aDate - bDate;
+                } else if (aTime.includes(':') && !bTime.includes(':')) {
+                    // aTime is a specific time, bTime is a duration in minutes.
+                    return -1;
+                } else if (!aTime.includes(':') && bTime.includes(':')) {
+                    // aTime is a duration in minutes, bTime is a specific time.
+                    return 1;
+                } else {
+                    // Both times are durations in minutes.
+                    return parseInt(aTime) - parseInt(bTime);
+                }
+                });
+        
         for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
+            let currentItem = itemsArray[i] 
             let currentItemID = currentItem[0]
             let currentItemValue = currentItem[1]
             
@@ -44,12 +77,19 @@ function clearShoppingListEl() {
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+    deptFieldEl.value = ""
+    taskFieldEl.value = ""
+    timeFieldEl.value = ""
+    
 }
 
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
-    let itemValue = item[1]
+    let tempValue = item[1]
+    
+    let itemValue = ""
+    itemValue += tempValue[1] + ": " + tempValue[2] + ": " + tempValue[0]
+    console.log(itemValue)
     
     let newEl = document.createElement("li")
     
